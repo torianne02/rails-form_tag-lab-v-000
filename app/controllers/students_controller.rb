878dbe95847a5662ffd12c3462033a1ae1eeef28
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-07 23:53:22 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,6 @@ class StudentsController < ApplicationController
   end 
 
   def create 
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to posts_path
+    render :new
   end
 end
